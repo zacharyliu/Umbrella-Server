3dad32414b80a153244e75f8cb6240b0cfe27587
Zachary Liu <liu.zachary@gmail.com>
2015-05-12 13:22:50 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,228 @@
+var DEBUG = false;
+
+var PORT = "/dev/cu.usbserial-A5025XYY";
+// var PORT = "/dev/cu.HC-06-DevB";
+
+if (!DEBUG) {
+  var SerialPort = require("serialport").SerialPort
+  var serialPort = new SerialPort(PORT, {
+    baudrate: 9600
+  });
+  var cobs = require("cobs");
+}
+
+var chroma = require("chroma-js");
+var Color = require("color");
+
+var LED_COUNT = 20;
+var LED_COUNT_NUM_ROWS = 5;
+var LED_COUNT_NUM_COLS = 4;
+var LED_MAP = [
+  [0, 1, 2, 3, 4],
+  [9, 8, 7, 6, 5],
+  [10, 11, 12, 13, 14],
+  [19, 18, 17, 16, 15]
+];
+
+function ColorArray() {
+  this.colors = [];
+  for (var i = 0; i < LED_COUNT; i++) {
+    this.colors.push([0x00, 0x00, 0x00]);
+  }
+};
+
+ColorArray.prototype.setAt = function (col, row, color) {
+  var index = LED_MAP[col][row];
+  this.colors[index] = color;
+  return this;
+}
+
+ColorArray.prototype.setCol = function (col, color) {
+  for (var i = 0; i < LED_COUNT_NUM_ROWS; i++) {
+    var index = LED_MAP[col][i];
+    this.colors[index] = color;
+  }
+  return this;
+}
+
+ColorArray.prototype.setRow = function (row, color) {
+  for (var i = 0; i < LED_COUNT_NUM_COLS; i++) {
+    var index = LED_MAP[i][row];
+    this.colors[index] = color;
+  }
+  return this;
+}
+
+ColorArray.prototype.setAll = function (color) {
+  for (var i = 0; i < LED_COUNT_NUM_COLS; i++) {
+    this.setCol(i, color);
+  }
+  return this;
+}
+
+ColorArray.prototype.toFlatArray = function () {
+  var flatArray = [];
+  for (var i = 0; i < LED_COUNT; i++) {
+    flatArray = flatArray.concat(this.colors[i]);
+  }
+  return flatArray;
+}
+
+ColorArray.prototype.toData = function (duration) {
+  // little-endian order
+  var intArray = [intToHex(duration & 0x000000ff),
+    intToHex((duration & 0x0000ff00) >> 8),
+    intToHex((duration & 0x00ff0000) >> 16),
+    intToHex((duration & 0xff000000) >> 24)];
+  var dataArray = intArray.concat(this.toFlatArray());
+  console.log(dataArray.join(" "));
+  return cobs.encode(new Buffer(dataArray));
+}
+
+ColorArray.prototype.toString = function () {
+  var allStrings = [];
+  var colors = this.toFlatArray();
+  for (var i = 0; i < colors.length / 3; i++) {
+    var string = "#";
+    for (var j = 0; j < 3; j++) {
+      var addString = colors[i * 3 + j].toString(16);
+      if (addString.length < 2) {
+        addString = "0" + addString;
+      }
+      string += addString;
+    }
+    allStrings.push(string);
+  }
+  return allStrings.join(" ");
+}
+
+function intToHex(num) {
+  return parseInt(num.toString(16), 16);
+}
+
+function rand255() {
+  return intToHex(Math.floor(255 * Math.random()));
+}
+
+function randColor() {
+  return [rand255(), rand255(), rand255()];
+}
+
+function multiColor(color, num) {
+  var all = [];
+  for (var i = 0; i < num; i++) {
+    all = all.concat(color);
+  }
+  return all;
+}
+
+var COLOR_SUNLIGHT = [0xFF, 0xE0, 0xE0];
+
+var patterns = {
+  "lightning": function () {
+    var col = Math.floor(Math.random() * LED_COUNT_NUM_COLS);
+    return [
+      [0,   new ColorArray().setCol(col, [0xDD, 0xDD, 0xDD]), 0],
+      [10,  new ColorArray().setCol(col, [0x50, 0x50, 0x50]), 0],
+      [0,   new ColorArray().setCol(col, [0xFF, 0xFF, 0xFF]), 0],
+      [200, new ColorArray(), function () {return 1000 + 2000*Math.random()}],
+    ]
+  },
+  "sunny": [[1000, new ColorArray().setAll(COLOR_SUNLIGHT), 1000], [1000, new ColorArray().setAll([0x00, 0x00, 0x00]), 1000]],
+  "sunset": [
+    [200, new ColorArray().setAll(COLOR_SUNLIGHT), 0],
+    [200, new ColorArray().setAll(Color().rgb([0xFA, 0xD6, 0xA5]).darken(0.2).rgbArray()), 0], // https://en.wikipedia.org/wiki/Sunset_%28color%29
+    [200, new ColorArray()
+      .setAll(   Color().rgb([0xFA, 0xD6, 0xA5]).darken(0.95).rgbArray())
+      .setCol(0, Color().rgb([0xFA, 0xD6, 0xA5]).darken(0.3).saturate(0.5).rgbArray())
+    , 0],
+    [200, new ColorArray().setAt(0, 0, Color().rgb([0xFA, 0xD6, 0xA5]).darken(0.3).saturate(0.5).rgbArray()), 0]
+  ]
+};
+
+function getData(duration, colors) {
+  var dataArray = [duration].concat(colors);
+  return cobs.encode(new Buffer(dataArray));
+}
+
+function colorsToString(colors) {
+  var allStrings = [];
+  for (var i = 0; i < colors.length / 3; i++) {
+    var string = "#";
+    for (var j = 0; j < 3; j++) {
+      var addString = colors[i * 3 + j].toString(16);
+      if (addString.length < 2) {
+        addString = "0" + addString;
+      }
+      string += addString;
+    }
+    allStrings.push(string);
+  }
+  return allStrings.join(" ");
+};
+
+var toggle = true;
+function loop(err) {
+  var duration = 500;
+
+  if (err) console.log(err);
+  // var color = randColor();
+
+  array = patterns["sunny"];
+  if (typeof array === 'function') array = array();
+
+  var delay = 0;
+  for (var i = 0; i < array.length; i++) {
+    (function () {
+      var duration = array[i][0];
+      if (typeof duration === 'function') duration = duration();
+
+      var colors = array[i][1];
+      if (typeof colors === 'function') colors = colors();
+
+      var hold = array[i][2];
+      if (typeof hold === 'function') hold = hold();
+
+      var thisDelay = delay;
+
+      setTimeout(function () {
+        if (!DEBUG) {
+          var data = colors.toData(duration);
+          serialPort.write(data);
+          serialPort.write([0x00]);
+        } else {
+          console.log("Delay:    " + thisDelay);
+          console.log("Duration: " + duration);
+          console.log("Colors:   " + colors.toString());
+          console.log("Hold:     " + hold);
+          console.log();
+        }
+      }, thisDelay);
+
+      delay += duration + hold;
+    })();
+  }
+
+  setTimeout(function() {
+    if (!DEBUG)
+      serialPort.drain(loop);
+    else
+      loop();
+  }, delay);
+}
+
+if (!DEBUG) {
+  console.log("Connecting to " + PORT + "...");
+  serialPort.on("open", function () {
+    console.log("Connected");
+
+    serialPort.on('data', function(data) {
+      console.log('data received: ' + data);
+    });
+
+    serialPort.write([0x00]);
+    serialPort.drain(loop);
+  });
+} else {
+  loop();
+}
---
+++ node_modules/cobs/index.js
@@ -0,0 +1,142 @@
+var stream = require('stream');
+
+function encode (buf, zeroFrame) {
+  var dest = [0];
+  var code_ptr = 0;
+  var code = 0x01;
+
+  if (zeroFrame) {
+    dest.push(0x00);
+    code_ptr++;
+  }
+
+  function finish (incllast) {
+    dest[code_ptr] = code;
+    code_ptr = dest.length;
+    incllast !== false && dest.push(0x00);
+    code = 0x01;
+  }
+
+  for (var i = 0; i < buf.length; i++) {
+    if (buf[i] == 0) {
+      finish();
+    } else {
+      dest.push(buf[i]);
+      code += 1;
+      if (code == 0xFF) {
+        finish();
+      }
+    }
+  }
+  finish(false);
+
+  if (zeroFrame) {
+    dest.push(0x00);
+  }
+
+  return new Buffer(dest);
+}
+
+
+function decode (buf)
+{
+  var dest = [];
+  for (var i = 0; i < buf.length; ) {
+    var code = buf[i++];
+    for (var j = 1; j < code; j++) {
+      dest.push(buf[i++]);
+    }
+    if (code < 0xFF && i < buf.length) {
+      dest.push(0);
+    }
+  }
+  return new Buffer(dest)
+}
+
+function encodeStream () {
+  var cobs = new stream.Duplex();
+  cobs.on('pipe', function (stream) {
+    stream.on('end', this.emit.bind(this, 'end'));
+  })
+  cobs._read = function (size) {
+  };
+  cobs._write = function (chunk, encoding, callback) {
+    chunk = encode(chunk, true)
+    this.push(chunk);
+    callback(null);
+  };
+  return cobs;
+}
+
+function clonebuffer (buf) {
+  return Buffer.concat([buf, new Buffer([])])
+}
+
+function splitter (next) {
+  var bufs = [];
+  return function (buf, encoding) {
+    buf = Buffer.isBuffer(buf) ? buf : new Buffer(buf, encoding);
+    for (var i = 0; i < buf.length; i++) {
+      if (buf[i] == 0) {
+        if (bufs.length) {
+          next(clonebuffer(Buffer.concat(bufs)));
+          bufs = [];
+        }
+      } else {
+        for (var j = i; j < buf.length; j++) {
+          if (buf[j] == 0) {
+            break;
+          }
+        }
+        bufs.push(clonebuffer(buf.slice(i, j)))
+        i = j - 1;
+      }
+    }
+  };
+}
+
+function decodeStream () {
+  var cobs = new stream.Duplex();
+  cobs.on('pipe', function (stream) {
+    stream.on('end', this.emit.bind(this, 'end'));
+  })
+  cobs._read = function (size) {
+  };
+  var pushsplit = splitter(function (chunk) {
+    var newchunk = decode(chunk)
+    cobs.push(newchunk);
+  })
+  cobs._write = function (chunk, encoding, callback) {
+    pushsplit(chunk, encoding);
+    callback(null);
+  };
+  return cobs;
+}
+
+module.exports = {
+  encode: encode,
+  decode: decode,
+  encodeStream: encodeStream,
+  decodeStream: decodeStream
+}
+
+// tests
+// function test (buf) {
+//  console.log(buf);
+//  console.log(stuff(buf))
+//  console.log(unstuff(stuff(buf)))
+//  console.log('')
+// }
+// test(new Buffer([0x00]))
+// test(new Buffer([0x11, 0x22, 0x00, 0x33]))
+// test(new Buffer([0x11, 0x00, 0x00, 0x00]))
+// test(new Buffer(Array.range(1, 255)))
+
+// var fs = require('fs');  
+// var buf = '';
+// fs.createReadStream('./index.js').pipe(encodeStream()).pipe(decodeStream()).on('data', function (data) {
+//  // console.error(String(data));
+//  buf += String(data);
+// }).on('end', function () {
+//  console.log('Success:', fs.readFileSync('./index.js', 'utf-8') == buf);
+// })
